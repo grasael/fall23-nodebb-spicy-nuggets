4df749761c60668a7d3a1bc87b3d181187a2b3f8
Krutika Kumar <tikakumar@krutikas-mbp.wifi.local.cmu.edu>
2023-10-12 16:40:03 -0400

    reverting previous commit to go back to the original version of user.js

--- src/posts/user.js
+++ src/posts/user.js
@@ -1,269 +1,104 @@
 'use strict';
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
+
+const _ = require('lodash');
+
+const db = require('../database');
+const utils = require('../utils');
+const user = require('../user');
+const privileges = require('../privileges');
+const plugins = require('../plugins');
+
+const Posts = module.exports;
+
+require('./data')(Posts);
+require('./create')(Posts);
+require('./delete')(Posts);
+require('./edit')(Posts);
+require('./parse')(Posts);
+require('./user')(Posts);
+require('./topics')(Posts);
+require('./category')(Posts);
+require('./summary')(Posts);
+require('./recent')(Posts);
+require('./tools')(Posts);
+require('./votes')(Posts);
+require('./bookmarks')(Posts);
+require('./queue')(Posts);
+require('./diffs')(Posts);
+require('./uploads')(Posts);
+
+Posts.exists = async function (pids) {
+    return await db.exists(
+        Array.isArray(pids) ? pids.map(pid => `post:${pid}`) : `post:${pids}`
+    );
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+
+Posts.getPidsFromSet = async function (set, start, stop, reverse) {
+    if (isNaN(start) || isNaN(stop)) {
+        return [];
+    }
+    return await db[reverse ? 'getSortedSetRevRange' : 'getSortedSetRange'](set, start, stop);
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-const async_1 = __importDefault(require("async"));
-const validator_1 = __importDefault(require("validator"));
-const lodash_1 = __importDefault(require("lodash"));
-const database_1 = __importDefault(require("../database"));
-const user_1 = __importDefault(require("../user"));
-const topics_1 = __importDefault(require("../topics"));
-const groups_1 = __importDefault(require("../groups"));
-const meta_1 = __importDefault(require("../meta"));
-const plugins_1 = __importDefault(require("../plugins"));
-const privileges_1 = __importDefault(require("../privileges"));
-function default_1(Posts) {
-    Posts.getUserInfoForPosts = function (uids, uid) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const [userData, userSettings, signatureUids] = yield Promise.all([
-                getUserData(uids, uid),
-                user_1.default.getMultipleUserSettings(uids),
-                privileges_1.default.global.filterUids('signature', uids),
-            ]);
-            const uidsSignatureSet = new Set(signatureUids.map(uid => parseInt(uid, 10)));
-            const groupsMap = yield getGroupsMap(userData);
-            userData.forEach((userData, index) => {
-                userData.signature = validator_1.default.escape(String(userData.signature || ''));
-                userData.fullname = userSettings[index].showfullname ? validator_1.default.escape(String(userData.fullname || '')) : undefined;
-                userData.selectedGroups = [];
-                if (meta_1.default.config.hideFullname) {
-                    userData.fullname = "Anonymous";
-                }
-            });
-            const result = yield Promise.all(userData.map((userData) => __awaiter(this, void 0, void 0, function* () {
-                const [isMemberOfGroups, signature, customProfileInfo] = yield Promise.all([
-                    checkGroupMembership(userData.uid, userData.groupTitleArray),
-                    parseSignature(userData, uid, uidsSignatureSet),
-                    plugins_1.default.hooks.fire('filter:posts.custom_profile_info', { profile: [], uid: userData.uid }),
-                ]);
-                if (isMemberOfGroups && userData.groupTitleArray) {
-                    userData.groupTitleArray.forEach((userGroup, index) => {
-                        if (isMemberOfGroups[index] && groupsMap[userGroup]) {
-                            userData.selectedGroups.push(groupsMap[userGroup]);
-                        }
-                    });
-                }
-                userData.signature = signature;
-                userData.custom_profile_info = customProfileInfo.profile;
-                return yield plugins_1.default.hooks.fire('filter:posts.modifyUserInfo', userData);
-            })));
-            const hookResult = yield plugins_1.default.hooks.fire('filter:posts.getUserInfoForPosts', { users: result });
-            return hookResult.users;
-        });
-    };
-    Posts.overrideGuestHandle = function (postData, handle) {
-        if (meta_1.default.config.allowGuestHandles && postData && postData.user && parseInt(postData.uid, 10) === 0 && handle) {
-            postData.user.username = validator_1.default.escape(String(handle));
-            if (postData.user.hasOwnProperty('fullname')) {
-                postData.user.fullname = postData.user.username;
-            }
-            postData.user.displayname = postData.user.username;
+
+Posts.getPostsByPids = async function (pids, uid) {
+    if (!Array.isArray(pids) || !pids.length) {
+        return [];
+    }
+    let posts = await Posts.getPostsData(pids);
+    posts = await Promise.all(posts.map(Posts.parsePost));
+    const data = await plugins.hooks.fire('filter:post.getPosts', { posts: posts, uid: uid });
+    if (!data || !Array.isArray(data.posts)) {
+        return [];
+    }
+    return data.posts.filter(Boolean);
+};
+
+Posts.getPostSummariesFromSet = async function (set, uid, start, stop) {
+    let pids = await db.getSortedSetRevRange(set, start, stop);
+    pids = await privileges.posts.filter('topics:read', pids, uid);
+    const posts = await Posts.getPostSummaryByPids(pids, uid, { stripTags: false });
+    return { posts: posts, nextStart: stop + 1 };
+};
+
+Posts.getPidIndex = async function (pid, tid, topicPostSort) {
+    const set = topicPostSort === 'most_votes' ? `tid:${tid}:posts:votes` : `tid:${tid}:posts`;
+    const reverse = topicPostSort === 'newest_to_oldest' || topicPostSort === 'most_votes';
+    const index = await db[reverse ? 'sortedSetRevRank' : 'sortedSetRank'](set, pid);
+    if (!utils.isNumber(index)) {
+        return 0;
+    }
+    return utils.isNumber(index) ? parseInt(index, 10) + 1 : 0;
+};
+
+Posts.getPostIndices = async function (posts, uid) {
+    if (!Array.isArray(posts) || !posts.length) {
+        return [];
+    }
+    const settings = await user.getSettings(uid);
+
+    const byVotes = settings.topicPostSort === 'most_votes';
+    let sets = posts.map(p => (byVotes ? `tid:${p.tid}:posts:votes` : `tid:${p.tid}:posts`));
+    const reverse = settings.topicPostSort === 'newest_to_oldest' || settings.topicPostSort === 'most_votes';
+
+    const uniqueSets = _.uniq(sets);
+    let method = reverse ? 'sortedSetsRevRanks' : 'sortedSetsRanks';
+    if (uniqueSets.length === 1) {
+        method = reverse ? 'sortedSetRevRanks' : 'sortedSetRanks';
+        sets = uniqueSets[0];
+    }
+
+    const pids = posts.map(post => post.pid);
+    const indices = await db[method](sets, pids);
+    return indices.map(index => (utils.isNumber(index) ? parseInt(index, 10) + 1 : 0));
+};
+
+Posts.modifyPostByPrivilege = function (post, privileges) {
+    if (post && post.deleted && !(post.selfPost || privileges['posts:view_deleted'])) {
+        post.content = '[[topic:post_is_deleted]]';
+        if (post.user) {
+            post.user.signature = '';
         }
-    };
-    function checkGroupMembership(uid, groupTitleArray) {
-        return __awaiter(this, void 0, void 0, function* () {
-            if (!Array.isArray(groupTitleArray) || !groupTitleArray.length) {
-                return null;
-            }
-            return yield groups_1.default.isMemberOfGroups(uid, groupTitleArray);
-        });
     }
-    function parseSignature(userData, uid, signatureUids) {
-        return __awaiter(this, void 0, void 0, function* () {
-            if (!userData.signature || !signatureUids.has(userData.uid) || meta_1.default.config.disableSignatures) {
-                return '';
-            }
-            const result = yield Posts.parseSignature(userData, uid);
-            return result.userData.signature;
-        });
-    }
-    function getGroupsMap(userData) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const groupTitles = lodash_1.default.uniq(lodash_1.default.flatten(userData.map(u => u && u.groupTitleArray)));
-            const groupsMap = {};
-            const groupsData = yield groups_1.default.getGroupsData(groupTitles);
-            groupsData.forEach((group) => {
-                if (group && group.userTitleEnabled && !group.hidden) {
-                    groupsMap[group.name] = {
-                        name: group.name,
-                        slug: group.slug,
-                        labelColor: group.labelColor,
-                        textColor: group.textColor,
-                        icon: group.icon,
-                        userTitle: group.userTitle,
-                    };
-                }
-            });
-            return groupsMap;
-        });
-    }
-    function getUserData(uids, uid) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const fields = [
-                'uid', 'username', 'fullname', 'userslug',
-                'reputation', 'postcount', 'topiccount', 'picture',
-                'signature', 'banned', 'banned:expire', 'status',
-                'lastonline', 'groupTitle', 'mutedUntil',
-            ];
-            const result = yield plugins_1.default.hooks.fire('filter:posts.addUserFields', {
-                fields: fields,
-                uid: uid,
-                uids: uids,
-            });
-            return yield user_1.default.getUsersFields(result.uids, lodash_1.default.uniq(result.fields));
-        });
-    }
-    Posts.isOwner = function (pids, uid) {
-        return __awaiter(this, void 0, void 0, function* () {
-            uid = parseInt(uid.toString(), 10);
-            const isArray = Array.isArray(pids);
-            const pidArray = isArray ? pids : [pids]; // Use type assertion here
-            if (uid <= 0) {
-                return isArray ? pidArray.map(() => false) : false;
-            }
-            const postData = yield Posts.getPostsFields(pidArray, ['uid']);
-            const result = postData.map((post) => post && post.uid === uid);
-            return isArray ? result : result[0];
-        });
-    };
-    Posts.isModerator = function (pids, uid) {
-        return __awaiter(this, void 0, void 0, function* () {
-            if (parseInt(uid.toString(), 10) <= 0) {
-                return pids.map(() => false);
-            }
-            const cids = yield Posts.getCidsByPids(pids);
-            return yield user_1.default.isModerator(uid, cids);
-        });
-    };
-    Posts.changeOwner = function (pids, toUid) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const exists = yield user_1.default.exists(toUid);
-            if (!exists) {
-                throw new Error('[[error:no-user]]');
-            }
-            let postData = yield Posts.getPostsFields(pids, [
-                'pid', 'tid', 'uid', 'content', 'deleted', 'timestamp', 'upvotes', 'downvotes',
-            ]);
-            postData = postData.filter((post) => post.pid && post.uid !== parseInt(toUid.toString(), 10));
-            pids = postData.map((post) => post.pid);
-            const cids = yield Posts.getCidsByPids(pids);
-            const bulkRemove = [];
-            const bulkAdd = [];
-            let repChange = 0;
-            const postsByUser = {};
-            postData.forEach((post, i) => {
-                post.cid = cids[i];
-                repChange += post.votes;
-                bulkRemove.push([`uid:${post.uid}:posts`, post.pid]);
-                bulkRemove.push([`cid:${post.cid}:uid:${post.uid}:pids`, post.pid]);
-                bulkRemove.push([`cid:${post.cid}:uid:${post.uid}:pids:votes`, post.pid]);
-                bulkAdd.push([`uid:${toUid}:posts`, post.timestamp, post.pid]);
-                bulkAdd.push([`cid:${post.cid}:uid:${toUid}:pids`, post.timestamp, post.pid]);
-                if (post.votes > 0 || post.votes < 0) {
-                    bulkAdd.push([`cid:${post.cid}:uid:${toUid}:pids:votes`, post.votes, post.pid]);
-                }
-                postsByUser[post.uid] = postsByUser[post.uid] || [];
-                postsByUser[post.uid].push(post);
-            });
-            yield Promise.all([
-                database_1.default.setObjectField(pids.map((pid) => `post:${pid}`), 'uid', toUid),
-                database_1.default.sortedSetRemoveBulk(bulkRemove),
-                database_1.default.sortedSetAddBulk(bulkAdd),
-                user_1.default.incrementUserReputationBy(toUid, repChange),
-                handleMainPidOwnerChange(postData, toUid),
-                updateTopicPosters(postData, toUid),
-            ]);
-            yield Promise.all([
-                user_1.default.updatePostCount(toUid),
-                reduceCounters(postsByUser),
-            ]);
-            plugins_1.default.hooks.fire('action:post.changeOwner', {
-                posts: lodash_1.default.cloneDeep(postData),
-                toUid: toUid,
-            });
-            return postData;
-        });
-    };
-    function reduceCounters(postsByUser) {
-        return __awaiter(this, void 0, void 0, function* () {
-            yield async_1.default.eachOfSeries(postsByUser, (posts, uid) => __awaiter(this, void 0, void 0, function* () {
-                const repChange = posts.reduce((acc, val) => acc + val.votes, 0);
-                yield Promise.all([
-                    user_1.default.updatePostCount(parseInt(uid, 10)),
-                    user_1.default.incrementUserReputationBy(parseInt(uid, 10), -repChange),
-                ]);
-            }));
-        });
-    }
-    function updateTopicPosters(postData, toUid) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const postsByTopic = lodash_1.default.groupBy(postData, (p) => parseInt(p.tid, 10));
-            yield async_1.default.eachOf(postsByTopic, (posts, tid) => __awaiter(this, void 0, void 0, function* () {
-                const postsByUser = lodash_1.default.groupBy(posts, (p) => parseInt(p.uid, 10));
-                yield database_1.default.sortedSetIncrBy(`tid:${tid}:posters`, posts.length, toUid);
-                yield async_1.default.eachOf(postsByUser, (userPosts, uid) => __awaiter(this, void 0, void 0, function* () {
-                    yield database_1.default.sortedSetIncrBy(`tid:${tid}:posters`, -userPosts.length, parseInt(uid, 10));
-                }));
-            }));
-        });
-    }
-    function handleMainPidOwnerChange(postData, toUid) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const tids = lodash_1.default.uniq(postData.map((p) => p.tid));
-            const topicData = yield topics_1.default.getTopicsFields(tids, [
-                'tid', 'cid', 'deleted', 'title', 'uid', 'mainPid', 'timestamp',
-            ]);
-            const tidToTopic = lodash_1.default.zipObject(tids, topicData);
-            const mainPosts = postData.filter((post) => post.pid === tidToTopic[post.tid].mainPid);
-            if (!mainPosts.length) {
-                return;
-            }
-            const bulkAdd = [];
-            const bulkRemove = [];
-            const postsByUser = {};
-            mainPosts.forEach((post) => {
-                bulkRemove.push([`cid:${post.cid}:uid:${post.uid}:tids`, post.tid]);
-                bulkRemove.push([`uid:${post.uid}:topics`, post.tid]);
-                bulkAdd.push([`cid:${post.cid}:uid:${toUid}:tids`, tidToTopic[post.tid].timestamp, post.tid]);
-                bulkAdd.push([`uid:${toUid}:topics`, tidToTopic[post.tid].timestamp, post.tid]);
-                postsByUser[post.uid] = postsByUser[post.uid] || [];
-                postsByUser[post.uid].push(post);
-            });
-            yield Promise.all([
-                database_1.default.setObjectField(mainPosts.map((p) => `topic:${p.tid}`), 'uid', toUid),
-                database_1.default.sortedSetRemoveBulk(bulkRemove),
-                database_1.default.sortedSetAddBulk(bulkAdd),
-                user_1.default.incrementUserFieldBy(toUid, 'topiccount', mainPosts.length),
-                reduceTopicCounts(postsByUser),
-            ]);
-            const changedTopics = mainPosts.map((p) => tidToTopic[p.tid]);
-            plugins_1.default.hooks.fire('action:topic.changeOwner', {
-                topics: lodash_1.default.cloneDeep(changedTopics),
-                toUid: toUid,
-            });
-        });
-    }
-    function reduceTopicCounts(postsByUser) {
-        return __awaiter(this, void 0, void 0, function* () {
-            yield async_1.default.eachSeries(Object.keys(postsByUser), (uid) => __awaiter(this, void 0, void 0, function* () {
-                const posts = postsByUser[uid];
-                const exists = yield user_1.default.exists(parseInt(uid, 10));
-                if (exists) {
-                    yield user_1.default.incrementUserFieldBy(parseInt(uid, 10), 'topiccount', -posts.length);
-                }
-            }));
-        });
-    }
-}
-exports.default = default_1;
-;
+};
+
+require('../promisify')(Posts);
